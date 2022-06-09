1a859e3a6cb476b68f51a8ada12cc6c83d558d4b
ionut florin <ionut@dianoia-it.com>
2022-06-09 12:38:28 +0300

    ExportPdf

--- ClientApp/src/app/propertyconfigurationitems/show-propertyconfigurationitems/show-propertyconfigurationitems.component.ts
+++ ClientApp/src/app/propertyconfigurationitems/show-propertyconfigurationitems/show-propertyconfigurationitems.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PropertyConfigurationItemsApiService } from 'src/app/Service/property-configuration-items-api.service';
+import jspdf from 'jspdf';  
+import html2canvas from 'html2canvas';
+
+
 @Component({
   selector: 'app-show-propertyconfigurationitems',
   templateUrl: './show-propertyconfigurationitems.component.html',
@@ -73,5 +77,24 @@ export class ShowPropertyconfigurationitemsComponent implements OnInit {
   clientModalClose() {
     this.activateAddEditpropConfigItemsComponent = false;
     this.propetyConfigurationItemsList$ = this.propertyConfigurationItemsService.getPropConfigItmByPropConfig(this.propConfigId);
+  }
+
+
+  
+  exportPdf(){
+    var data= document.getElementById('contentToConvert')!;  
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 208;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('MYPdf.pdf'); // Generated PDF   
+    });  
   }
 }
